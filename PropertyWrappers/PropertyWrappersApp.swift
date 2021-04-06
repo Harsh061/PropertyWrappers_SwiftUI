3ff0c97bb1fa49143ec12da269df24e4ca2c10c3
Harshit Parikh <harshit.parikh@bambooapp.com>
2021-04-06 10:58:34 +0530

    Initial Commit

---
+++ PropertyWrappers/PropertyWrappersApp.swift
@@ -0,0 +1,17 @@
+//
+//  PropertyWrappersApp.swift
+//  PropertyWrappers
+//
+//  Created by Harshit Parikh on 06/04/21.
+//
+
+import SwiftUI
+
+@main
+struct PropertyWrappersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
